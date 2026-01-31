81d323be393549a34d5646ef48a5690848039f67
builab <huy.bui@mcgill.ca>
2026-01-30 21:57:49 -0500

    Update default_config.py

--- src/default_config.py
+++ src/default_config.py
@@ -41,7 +41,7 @@ PRIMARYCILIA_TEMPLATE = 'primarycilia_template.csv'
 # 3D printing base
 THREEDPRINT = False
 THREEDPRINT_BASE_HEIGHT = 200
-THREEDPRINT_BASE_RADIUS = 1200
+THREEDPRINT_BASE_RADIUS = 1250
 
 # ============================================================================
 # CILIA DEFAULTS (9+2 Configuration)
